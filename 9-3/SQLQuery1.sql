49339a44ad7021aede3d0180d15c2f031200f583
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-09 11:30:16 +0100

    made function

--- 9-3/SQLQuery1.sql
+++ 9-3/SQLQuery1.sql
@@ -23,8 +23,36 @@ set @decimal=(@rand * 10);
 return @decimal;
 END;
 
+SELECT dbo.Randv2(rand())
+
+ALTER FUNCTION "Randv2" (@rand float) --ne zelim int nego decimalu
+returns float
+AS
+BEGIN
+declare @decimal integer;
+set @decimal=(@rand * 10);
+return @decimal;
+END;
+
 INSERT INTO Category VALUES
 (newid(), 'Horror', 'Fiction', (SELECT dbo.Randv2(rand()))),
-(newid(), 'Fantasy', 'Fiction', '5.67'),
-(newid(), 'Historical', 'Nonfiction','),
-(newid(), 'Documentary', 'Nonfiction');
+(newid(), 'Fantasy', 'Fiction', (SELECT dbo.Randv2(rand()))),
+(newid(), 'Historical', NULL, (SELECT dbo.Randv2(rand()))),
+(newid(), 'Documentary', 'Nonfiction', (SELECT dbo.Randv2(rand()))),
+(newid(), 'Romance', NULL, (SELECT dbo.Randv2(rand())));
+
+INSERT INTO Movie VALUES
+(newid(), 'Cabin in the Woods','1987', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Horror')),
+(newid(), 'Titanic', '1996', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Historical')),
+(newid(), 'Harambe', '2016', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Documentary')),
+(newid(), '2012', '2012', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Fantasy')),
+(newid(), 'Lord of the Rings','2001', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Fantasy')),
+(newid(), 'Resident Evil','2013', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Horror')),
+(newid(), 'World War 2','2000', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Historical')),
+(newid(), 'Kingdom of Heaven', NULL, (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Horror')),
+(newid(), 'Batman', '1996', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Historical')),
+(newid(), 'Harambe', '2016', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Documentary')),
+(newid(), '2012', '2012', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Fantasy')),
+(newid(), 'Lord of the Rings','2001', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Fantasy')),
+(newid(), 'Resident Evil','2013', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Horror')),
+(newid(), 'World War 2','2000', (SELECT dbo.Randv2(rand())), (SELECT "Id" FROM "Category" WHERE "Genre"='Historical'));
