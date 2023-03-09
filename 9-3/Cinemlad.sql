e297be9defd72e1531adbeed3b2eb2894a256a5e
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-09 14:27:54 +0100

    start new db for future use

---
+++ 9-3/Cinemlad.sql
@@ -0,0 +1,11 @@
+﻿CREATE DATABASE Cinemlad
+
+CREATE TABLE Showing (
+Id uniqueidentifier not null PRIMARY KEY,
+Title varchar(50) not null,
+Release int not null,
+Genre varchar(50) null,
+Duration int not null,
+ShowingStart date not null, --početak prikazivanja u godini
+ShowingEnd date not null,
+StartTime time not null); --u koliko sati taj dan se prikazuje
