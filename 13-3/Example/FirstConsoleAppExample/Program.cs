397a1e2c8b60c06d48543c9429c7171af95975ce
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-13 09:34:11 +0100

    commit but moving to new repository from this point

--- 13-3/Example/FirstConsoleAppExample/Program.cs
+++ 13-3/Example/FirstConsoleAppExample/Program.cs
@@ -10,6 +10,33 @@ namespace FirstConsoleAppExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello WOrld!");
+
+            string name = "Jelena";
+            Console.WriteLine(name);
+
+            //deklaracija
+            int a, b, c, d;
+            //inicijalizacija
+            a = 1;
+            b = 6;
+            c = d = 7;
+
+            Console.WriteLine($"a={a}\n b={b}\n c={c}\n d={d}\n"); //mora ići $ inače doslovno ispisuje a={a}
+
+            bool isActive = false;
+
+            if(isActive)
+            {
+                Console.WriteLine(name);
+            }
+            else
+            {
+                string secondName ="Ana";
+                Console.WriteLine (secondName); //ovaj secondname varijabla postoji samo unutar ove else funkcije jer je tu definirana
+            }
+
+            Console.ReadLine();
         }
     }
 }
